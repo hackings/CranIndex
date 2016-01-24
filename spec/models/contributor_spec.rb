8ac15d25b4069f2417aa065be7c815c464eb8b0c
Vishnu Atrai <me@vishnuatrai.com>
2016-01-24 23:19:47 +0530

    spec for authors and maintainers name with email

--- spec/models/contributor_spec.rb
+++ spec/models/contributor_spec.rb
@@ -10,4 +10,15 @@ RSpec.describe Contributor, type: :model do
   	should have_many( :package_contributors )
   }
 
+  #Instance Methods
+  it "should return `name_with_email` only name for email blank" do 
+    contributor = FactoryGirl.create(:contributor, name: 'Foo Bar', email: nil)
+    expect(contributor.name_with_email).to eq("Foo Bar")
+  end   
+
+  it "should return `name_with_email` both name and email" do 
+    contributor = FactoryGirl.create(:contributor, name: 'John Doh', email: 'john@sample.com')
+    expect(contributor.name_with_email).to eq("John Doh <john@sample.com>")
+  end   
+
 end
--- spec/models/package_spec.rb
+++ spec/models/package_spec.rb
@@ -12,4 +12,24 @@ RSpec.describe Package, type: :model do
   	should have_many( :package_authors ).through( :package_contributors ).source( :contributor).conditions( package_contributors: { contributor_type: 'Author'} )
   	should have_many( :package_maintainers ).through( :package_contributors ).source( :contributor).conditions( package_contributors: { contributor_type: 'Maintainer'} )
   }
+
+  #Instance Methods
+  it "should return `authers_name` for package" do 
+    autor1 = FactoryGirl.create(:contributor, name: 'John Doh', email: 'john@sample.com')
+    autor2 = FactoryGirl.create(:contributor, name: 'Foo Bar', email: 'foo@bar.com')
+    package = FactoryGirl.create(:package)
+    package.package_authors << autor1
+    package.package_authors << autor2
+    expect(package.authers_name).to eq("John Doh <john@sample.com>, Foo Bar <foo@bar.com>")
+  end   
+
+  it "should return `maintainers_name` for package" do 
+    maintainer1 = FactoryGirl.create(:contributor, name: 'John Doh', email: 'john@sample.com')
+    maintainer2 = FactoryGirl.create(:contributor, name: 'Foo Bar', email: 'foo@bar.com')
+    package = FactoryGirl.create(:package)
+    package.package_maintainers << maintainer1
+    package.package_maintainers << maintainer2
+    expect(package.maintainers_name).to eq("John Doh <john@sample.com>, Foo Bar <foo@bar.com>")
+  end   
+
 end
